8e2a4030802317602b4d9aa09661cd36a663efab
matsurihime <pipikapu@gmail.com>
2020-01-15 23:40:43 +0900

    fix test style

--- test/test_pyjstage.py
+++ test/test_pyjstage.py
@@ -2,11 +2,15 @@ from src.pyjstage import Pyjstage
 from service import Service
 from status import Status
 from result import Result
+from unittest import TestCase
 
 
-class TestPyjstage:
-    def __init__(self):
+class TestPyjstage(TestCase):
+    def setUp(self):
         self.pyjstage = Pyjstage()
+
+    def tearDown(self):
+        pass
 
     @staticmethod
     def commons(ret: Result):
@@ -18,18 +22,18 @@ class TestPyjstage:
     def test_list(self):
         ret = self.pyjstage.list(issn='2186-6619')
         self.commons(ret)
-        assert ret.link == 'http://api.jstage.jst.go.jp/searchapi/do?service=2&issn=2186-6619'
-        assert ret.servicecd == Service.LIST.value
-        assert ret.items_per_page > 0
-        assert len(ret.entries) > 0
+        self.assertEqual(ret.link, 'http://api.jstage.jst.go.jp/searchapi/do?service=2&issn=2186-6619')
+        self.assertEqual(ret.servicecd, Service.LIST.value)
+        self.assertTrue(ret.items_per_page > 0)
+        self.assertTrue(len(ret.entries) > 0)
 
     def test_search(self):
         ret = self.pyjstage.search(issn='2186-6619', count=1)
         self.commons(ret)
-        assert ret.link == 'http://api.jstage.jst.go.jp/searchapi/do?service=3&issn=2186-6619&count=1'
-        assert ret.servicecd == Service.SEARCH.value
-        assert ret.items_per_page == 1
-        assert len(ret.entries) == 1
+        self.assertEqual(ret.link, 'http://api.jstage.jst.go.jp/searchapi/do?service=3&issn=2186-6619&count=1')
+        self.assertEqual(ret.servicecd, Service.SEARCH.value)
+        self.assertEqual(ret.items_per_page, 1)
+        self.assertEqual(len(ret.entries), 1)
 
     def test_build_query(self):
         query = self.pyjstage.build_query(
@@ -41,8 +45,11 @@ class TestPyjstage:
             cdjournal='cdjournal',
             volorder='volorder'
         )
-        assert query == 'http://api.jstage.jst.go.jp/searchapi/do?service=2&pubyearfrom=2017&pubyearto=2018' \
-                        '&material=material&issn=issn&cdjournal=cdjournal&volorder=volorder'
+        self.assertEqual(
+            query,
+            'http://api.jstage.jst.go.jp/searchapi/do?service=2&pubyearfrom=2017&pubyearto=2018&material=material'
+            '&issn=issn&cdjournal=cdjournal&volorder=volorder'
+        )
 
         query = self.pyjstage.build_query(
             service=Service.SEARCH.value,
@@ -63,6 +70,9 @@ class TestPyjstage:
             start='start',
             count='count'
         )
-        assert query == 'http://api.jstage.jst.go.jp/searchapi/do?service=3&pubyearfrom=2015&pubyearto=2019' \
-                        '&material=material&article=article&author=author&affile=affile&keyword=keyword&abst=abst' \
-                        '&text=text&issn=issn&cdjournal=cdjournal&sortfig=sortfig&vol=vol&no=no&start=start&count=count'
+        self.assertEqual(
+            query,
+            'http://api.jstage.jst.go.jp/searchapi/do?service=3&pubyearfrom=2015&pubyearto=2019&material=material'
+            '&article=article&author=author&affile=affile&keyword=keyword&abst=abst&text=text&issn=issn'
+            '&cdjournal=cdjournal&sortfig=sortfig&vol=vol&no=no&start=start&count=count'
+        )
--- test/test_result.py
+++ test/test_result.py
@@ -1,32 +1,36 @@
 from parser import Parser
 from service import Service
 from status import Status
+from unittest import TestCase
 
 
-class TestResult:
-    def __init__(self):
+class TestResult(TestCase):
+    def setUp(self):
         self.parser = Parser()
+
+    def tearDown(self):
+        pass
 
     def test_parse_search(self):
         with open('resources/search.xml', 'r') as f:
             xml_result = f.read()
         ret = self.parser.parse(xml_result.encode('utf-8'))
-        assert ret.status == Status.OK.value
-        assert ret.servicecd == Service.SEARCH.value
-        assert ret.message is None
-        assert ret.total_results > 0
-        assert ret.start_index == 1
-        assert ret.items_per_page == 3
-        assert len(ret.entries) == 3
+        self.assertEqual(ret.status, Status.OK.value)
+        self.assertEqual(ret.servicecd, Service.SEARCH.value)
+        self.assertTrue(ret.message is None)
+        self.assertTrue(ret.total_results > 0)
+        self.assertEqual(ret.start_index, 1)
+        self.assertEqual(ret.items_per_page, 3)
+        self.assertEqual(len(ret.entries), 3)
 
     def test_parse_list(self):
         with open('resources/list.xml', 'r') as f:
             xml_result = f.read()
         ret = self.parser.parse(xml_result.encode('utf-8'))
-        assert ret.status == Status.OK.value
-        assert ret.servicecd == Service.LIST.value
-        assert ret.message is None
-        assert ret.total_results > 0
-        assert ret.start_index == 1
-        assert ret.items_per_page == 1
-        assert len(ret.entries) == 1
+        self.assertEqual(ret.status, Status.OK.value)
+        self.assertEqual(ret.servicecd, Service.LIST.value)
+        self.assertTrue(ret.message is None)
+        self.assertTrue(ret.total_results > 0)
+        self.assertEqual(ret.start_index, 1)
+        self.assertEqual(ret.items_per_page, 1)
+        self.assertEqual(len(ret.entries), 1)
